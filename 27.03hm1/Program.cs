4b58ccf6ffd857b79e05d0ae7b8b99d7c3572890
Artem <artemzhdanov1299@gmail.com>
2023-03-07 01:24:11 +0200

    Add project files.

---
+++ 27.03hm1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._02hm1
+{
+    class Num<Z>
+    {
+        private Z x;
+        private Z c;
+
+        public Num(Z x, Z c)
+        {
+            this.x = x;
+            this.c = c;
+        }
+        public void Swap()
+        {
+            Z v = x;
+            x = c;
+            c = v;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"a = {x}\nb = {c}");
+        }
+    }
+    internal class Program
+    {
+        public static Z Swap<Z>(Z x, Z c) where Z : IComparable<Z>
+        {
+            Z v = x;
+            x = c;
+            c = v;
+            return default(Z);
+        }
+        static void Main(string[] args)
+        {
+            Num<int> num = new Num<int>(1, 5);
+            num.Swap();
+            num.Print();
+        }
+    }
+}
